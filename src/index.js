c72b79997d68bdde1731c53a9c7278aec6201a88
George Volozhinets <bseries13@gmail.com>
2020-03-07 16:19:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsConfigLength = bracketsConfig.length
+
+  for (let i = 0; i < bracketsConfigLength;){
+    let brackets = bracketsConfig[i].join('');
+    if (str.includes(brackets)){
+      str = str.replace(brackets, '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  if (result = str.length){
+    return false;
+  }else{
+    return true;
+  }
+  return result;
 }
